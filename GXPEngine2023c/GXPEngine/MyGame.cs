d0363458e47e725a0c40ca2b771928844c564a3e
Juliette Landman <Juliettelandman@gmail.com>
2024-02-13 14:36:04 +0100

    Update GXP

--- GXPEngine2023c/GXPEngine/MyGame.cs
+++ GXPEngine2023c/GXPEngine/MyGame.cs
@@ -1,11 +1,11 @@
-using System;                                   // System contains a lot of default C# libraries 
-using GXPEngine;                                // GXPEngine contains the engine
-using System.Drawing;                           // System.Drawing contains drawing tools such as Color definitions
+using System;                             
+using GXPEngine;                               
+using System.Drawing;                           
 
 public class MyGame : Game {
-	public MyGame() : base(800, 600, false)     // Create a window that's 800x600 and NOT fullscreen
+	public MyGame() : base(800, 600, false)     
 	{
-		// Draw some things on a canvas:
+
 		EasyDraw canvas = new EasyDraw(800, 600);
 		canvas.Clear(Color.MediumPurple);
 		canvas.Fill(Color.Yellow);
@@ -20,13 +20,12 @@ public class MyGame : Game {
 		Console.WriteLine("MyGame initialized");
 	}
 
-	// For every game object, Update is called every frame, by the engine:
 	void Update() {
-		// Empty
+	
 	}
 
-	static void Main()                          // Main() is the first method that's called when the program is run
+	static void Main()                    
 	{
-		new MyGame().Start();                   // Create a "MyGame" and start it
+		new MyGame().Start();            
 	}
 }
