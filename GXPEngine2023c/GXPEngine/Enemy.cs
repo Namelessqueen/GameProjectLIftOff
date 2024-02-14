fd3d73c73258e9e3abbde7c094f167775f271d22
Bram <bram.salomons@gmail.com>
2024-02-14 15:04:51 +0100

    Added enemy functionality

---
+++ GXPEngine2023c/GXPEngine/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using GXPEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Enemy : AnimationSprite
+{
+
+
+    public Enemy(string fileName, int cols, int rows) : base(fileName, cols, rows)
+    {
+
+        SetOrigin(width / 2, height / 2);
+    }
+
+
+
+    public void Update()
+    {
+        Act();
+    }
+
+
+
+
+    protected virtual void Act()
+    {
+        Console.WriteLine("Behavior.Act: not implemented");
+        // Even better: make this an *abstract* method to force implementing it in subclasses!
+    }
+}
---
+++ GXPEngine2023c/GXPEngine/ShootingEnemy.cs
@@ -0,0 +1,80 @@
+﻿using GXPEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class ShootingEnemy : Enemy
+{
+
+    private float speed = 1;
+    private Sprite target;
+    private float bulletCooldown = 1f;
+    private float bulletSpeed = 3;
+    private float time;
+    private float xPointToPlayer, yPointToPlayer;
+
+
+    public ShootingEnemy(string fileName, int cols, int rows) : base("checkers.png", cols, rows)
+    {
+        scale = 1;
+        SetOrigin(width / 2, height / 2);
+
+
+    }
+
+
+    protected override void Act()
+    {
+
+        if (target == null) target = game.FindObjectOfType<Player>();
+
+        /**/
+        // this version looks smoother
+        xPointToPlayer = (target.x - x) / DistanceTo(target);
+        yPointToPlayer = (target.y - y) / DistanceTo(target);
+
+        /*
+        // this version uses the pythagoran theorem but looks worse
+        xPointToPlayer = Mathf.Pow((target.x - x), 2) / Mathf.Pow(DistanceTo(target), 2);
+        yPointToPlayer = Mathf.Pow((target.y - y), 2) / Mathf.Pow(DistanceTo(target), 2);
+        
+        if (target.x < x) xPointToPlayer *= -1;
+        if (target.y < y) yPointToPlayer *= -1;
+        /**/
+
+        Console.WriteLine(Mathf.Abs(Mathf.Abs(xPointToPlayer) + Mathf.Abs(yPointToPlayer) - 1));
+
+
+
+
+
+        // SHOOTING
+        time += Time.deltaTime;
+        //Console.WriteLine(time);
+
+        if (time / 1000 >= bulletCooldown)
+        {
+            AddChild(new Bullet(bulletSpeed * xPointToPlayer, bulletSpeed * yPointToPlayer));
+            time -= bulletCooldown * 1000;
+        }
+
+
+        // MOVEMENT
+        // if enemy is close enough to player, stop moving
+        if (DistanceTo(target) <= 150) return;
+
+
+        x += speed * xPointToPlayer;
+        y += speed * yPointToPlayer;
+
+
+
+    }
+
+
+
+
+
+}
